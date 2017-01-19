df8f0b9a2aee67a0a79b71e54a6e05cf0fff50c6
choihyesu <1004choihs@naver.com>
2017-01-19 14:07:14 +0900

    hyesutest

--- hyesutest/main.js
+++ hyesutest/main.js
@@ -1,62 +1,24 @@
-/* global __dirname */
-/* global process */
-var app = require('app');  // Module to control application life.
-var BrowserWindow = require('browser-window');  // Module to create native browser window.
-
-// Report crashes to our server.
-require('crash-reporter').start();
-
-// Prevent the computer from going to sleep
-const powerSaveBlocker = require('electron').powerSaveBlocker;
-var id = powerSaveBlocker.start('prevent-display-sleep');
-console.log(powerSaveBlocker.isStarted(id));
+'use strict';
+const electron = require('electron');
+// Module to control application life.
+const app = electron.app;
+// Module to create native browser window.
+const BrowserWindow = electron.BrowserWindow;
 
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
-var mainWindow = null;
+let mainWindow;
 
-// Quit when all windows are closed.
-app.on('window-all-closed', function() {
-  // On OS X it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform != 'darwin') {
-    app.quit();
-  }
-});
-
-// This method will be called when Electron has finished
-// initialization and is ready to create browser windows.
-app.on('ready', function() {
-
-
-  // Put the app on a secondary display if availalbe
-  var atomScreen = require('screen');
-  var displays = atomScreen.getAllDisplays();
-  var externalDisplay = null;
-  for (var i in displays) {
-    if (displays[i].bounds.x > 0 || displays[i].bounds.y > 0) {
-      externalDisplay = displays[i];
-      break;
-    }
-  }
-
-  var browserWindowOptions = {width: 800, height: 600, icon: 'favicon.ico' , kiosk:true, autoHideMenuBar:true, darkTheme:true};
-  if (externalDisplay) {
-    browserWindowOptions.x = externalDisplay.bounds.x + 50;
-    browserWindowOptions.y = externalDisplay.bounds.y + 50
-  }
-
+function createWindow () {
   // Create the browser window.
-  mainWindow = new BrowserWindow(browserWindowOptions);
+  var browserWindowOptions = {width: 800, height: 600, icon: 'favicon.ico' , kiosk:true, autoHideMenuBar:true, darkTheme:true};
+  mainWindow = new BrowserWindow(browserWindowOptions); //{width: 800, height: 600}
 
   // and load the index.html of the app.
-  mainWindow.loadUrl('file://' + __dirname + '/index.html');
+  mainWindow.loadURL('file://' + __dirname + '/index.html');
 
-  // Open the DevTools if run with "npm start dev"
-  if(process.argv[2] == "dev"){
-    mainWindow.webContents.openDevTools();
-  }
-
+  // Open the DevTools.
+  //mainWindow.webContents.openDevTools();
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function() {
@@ -65,4 +27,23 @@ app.on('ready', function() {
     // when you should delete the corresponding element.
     mainWindow = null;
   });
+}
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.on('ready', createWindow);
+
+// Quit when all windows are closed.
+app.on('window-all-closed', function () {
+  // On OS X it is common for applications and their menu bar
+  // to stay active until the user quits explicitly with Cmd + Q
+  if (process.platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', function () {
+  if (mainWindow === null) {
+    createWindow();
+  }
 });
